72167a6b668e1e3f94f04facccb45cb4cc80e484
yaasir cheekoory <yaasir1997@gmail.com>
2024-02-13 02:31:33 +0400

    navigation component created

---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <div class="nav-container">
+    <p>MEDICA</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.nav-container {
+  background-color: aliceblue;
+  height: 8vh;
+  padding-inline: 2rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  p {
+    font-size: large;
+    font-weight: 600;
+  }
+}
+</style>
